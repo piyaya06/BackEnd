b1819ef28affaa99d8054aa5574bd3a1c7941109
piyaya06 <monoapril7@gmail.com>
2025-03-03 09:48:38 +0800

    menangani file upload dengan middleware

--- routers.js
+++ routers.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const routers = express.Router(); //tujuannya agar jika kita akan tambah router nanti akan masuk ke file router
 const path = require("path");
+const fs = require("fs"); //file upload
+const multer = require("multer"); //file upload
+//const upload = multer({ dest: "public" }); //file upload
 
-//ROUTING
+//File Upload tapi yang di upload hanya gambar saja
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ dest: "public", fileFilter: imageFilter });
+
+//ROUTING file upload menggunakan multer (boleh upload file, docs, gambar)
+routers.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  if (file) {
+    const target = path.join(__dirname, "public", file.originalname);
+    fs.renameSync(file.path, target);
+    res.send("file berhasil di upload");
+  } else {
+    res.send("file gagal diupload");
+  }
+});
+
 // //routing download (cara pendek)
 // routers.get("/download", (req, res) => {
 //   const filename = "logo.png";
 //   res.sendFile(__dirname + "/download/" + filename); //kalau lebih dari 1 foto tidak perlu /download/ langsung filename saja
 // });
 
-// //cara pendek method download
+// Download file private di web
 routers.get("/download", (req, res) => {
   const filename = "logo.png";
   res.sendFile(path.join(__dirname, "/download", filename), {
